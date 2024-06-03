460cdc3f13de23908b75c7924a53d4b5c938a69c
GreenMoCh <moadchabaa0@gmail.com>
2024-06-03 15:07:40 +0000

    fix

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-    res.type('text').send('Hello Horlberton School!');
+    res.type('text').send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
