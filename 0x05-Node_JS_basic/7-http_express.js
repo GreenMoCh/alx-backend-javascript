db1f3c33d2df9c73096c0fc912da39dbd2a2afde
GreenMoCh <125460183+GreenMoCh@users.noreply.github.com>
2024-06-03 16:12:37 +0100

    Update 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -2,31 +2,15 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
-const port = 1245;
-
-app.get('/', (req, res) => {
-    res.type('text').send('Hello Holberton School!');
-});
-
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
 app.get('/students', async (req, res) => {
-    try {
-        const studenst = await countStudents(process.argv[2]);
-
-        let response = 'This is the list of our students\n';
-        response += `Number of students: ${students.total}\n`;
-
-        for (const [field, names] of Object.entries(students.fields)) {
-            response += `Number of students in ${field}: ${name.length}. List: ${names.join(', ')}\n`;
-        }
-
-        res.type('text').send(response);
-    } catch (error) {
-        res.type('text').send('Cannot load the database');
-    }
+  const title = 'This is the list of our students\n';
+  try {
+    const data = await countStudents(process.argv[2]);
+    res.send(`${title}${data.join('\n')}`);
+  } catch (error) {
+    res.send(`${title}${error.message}`);
+  }
 });
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
-
-module.experts = app;
+app.listen(1245);
+module.exports = app;
